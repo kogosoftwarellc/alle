39f207dc0d745e1acccb77ab009d5c2fdc513b7f
jsdevel <js.developer.undefined@gmail.com>
2018-03-17 02:02:33 -0700

    Adding coverage to eslintignore and bumping coverage for list-packages helper.

--- src/helpers/__tests__/list-packages.test.js
+++ src/helpers/__tests__/list-packages.test.js
@@ -5,6 +5,7 @@ import listPackages from "../list-packages";
 describe("listPackages", () => {
   [
     ["when given a repo with a valid package", constants.repoValid, 1],
+    ["when not given a directory", "/", 0],
     ["when given a repo with no packages", constants.repoValidNoPackages, 0]
   ].forEach(test => {
     context(test[0], () => {
