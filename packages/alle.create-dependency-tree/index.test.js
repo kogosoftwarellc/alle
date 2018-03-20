96384951b29e16d7f6a6f30b12f450d4858bc075
jsdevel <js.developer.undefined@gmail.com>
2018-03-20 00:35:24 -0700

    Adding alle.create-dependency-tree (closes #8).
    * Fixing eslint failing on fixtures.
    * Removing node 5 and 4 builds from .travis.yml.

---
+++ packages/alle.create-dependency-tree/index.test.js
@@ -0,0 +1,48 @@
+import createDependencyTree from "alle.create-dependency-tree";
+
+describe("createDependencyTree", () => {
+  [
+    ["when not given an array", null, {}],
+    ["when not given any packages", [], {}],
+    [
+      "when given packages with no dependencies",
+      [
+        {
+          name: "foo"
+        },
+        {
+          name: "doo"
+        }
+      ],
+      {
+        doo: [],
+        foo: []
+      }
+    ],
+    [
+      "when given packages with dependencies",
+      [
+        {
+          name: "foo",
+          dependencies: {
+            doo: "*",
+            lodash: "*"
+          }
+        },
+        {
+          name: "doo"
+        }
+      ],
+      {
+        doo: [],
+        foo: ["doo"]
+      }
+    ]
+  ].forEach(test => {
+    context(test[0], () => {
+      it(`should output an expected tree`, () => {
+        expect(createDependencyTree(test[1])).toEqual(test[2]);
+      });
+    });
+  });
+});
